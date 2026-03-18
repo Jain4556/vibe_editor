2d434af5f2fe9bb0620e77496516218d66033415
Jain4556 <sunil925business@gmail.com>
2026-03-18 15:47:03 +0530

    03-Authentication

--- lib/db.ts
+++ lib/db.ts
@@ -1,22 +1,18 @@
 import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
-
-// 👇 create a function so type is inferred correctly
-const createPrismaClient = () => {
-  return new PrismaClient({
-    accelerateUrl: process.env.PRISMA_ACCELERATE_URL!,
-  }).$extends(withAccelerate());
-};
-
-// 👇 infer correct type
-type PrismaClientType = ReturnType<typeof createPrismaClient>;
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientType | undefined;
+  prisma: PrismaClient | undefined;
 };
 
 export const db =
-  globalForPrisma.prisma ?? createPrismaClient();
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL!,
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = db;
