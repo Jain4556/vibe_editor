5d5ee4d5cf9e0120eac2a2ed104ab0a8ed49ccdc
Jain4556 <sunil925business@gmail.com>
2026-03-18 14:12:22 +0530

    added layout and updated schema

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,23 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+// 👇 create a function so type is inferred correctly
+const createPrismaClient = () => {
+  return new PrismaClient({
+    accelerateUrl: process.env.PRISMA_ACCELERATE_URL!,
+  }).$extends(withAccelerate());
+};
+
+// 👇 infer correct type
+type PrismaClientType = ReturnType<typeof createPrismaClient>;
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+  prisma: PrismaClientType | undefined;
+};
 
 export const db =
-  globalForPrisma.prisma ?? new PrismaClient()
+  globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = db
+  globalForPrisma.prisma = db;
 }
